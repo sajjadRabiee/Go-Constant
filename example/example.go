b750ed7981bd645d7a1636301496816fb853836b
Sajjad <rabiee.614@gmail.com>
2023-12-26 14:39:40 +0330

    init

---
+++ example/example.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	constant "Constant"
+	"fmt"
+)
+
+func main() {
+	toConst, err := constant.ToConst[foo]("m4")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	toConst, err = constant.ToConst[foo]("m2")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(toConst)
+}
+
+type foo string
+
+func (f foo) String() string {
+	return string(f)
+}
+
+func (f foo) GetMembers() []foo {
+	return []foo{
+		m1,
+		m2,
+		m3,
+	}
+}
+
+func (f foo) GetDefault() foo {
+	return m1
+}
+
+const (
+	m1 foo = "m1"
+	m2 foo = "m2"
+	m3 foo = "m3"
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Constant
+
+go 1.18
